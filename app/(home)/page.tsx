7b674d12864ed359555829a73d989fdcfa7abfe9
Felipe Gomes <contato@felipegomes.me>
2024-01-31 22:50:16 -0300

    feat: adds header and welcome message

---
+++ app/(home)/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Header from "../_components/header";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Felipe!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", { locale: ptBR })}
+        </p>
+      </div>
+    </>
+  );
+}
---
+++ app/_components/header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+
+export default function Header() {
+  return (
+    <Card>
+      <CardContent className="p-5 flex justify-between items-center">
+        <h1>booking.now</h1>
+        <Button variant="outline" size="icon" className="size-8">
+          <MenuIcon size={18} />
+        </Button>
+      </CardContent>
+    </Card>
+  );
+}
