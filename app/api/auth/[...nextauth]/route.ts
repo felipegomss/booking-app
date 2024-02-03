746b18350232975b353c4d0ab4add25a3a4cf503
Felipe Gomes <contato@felipegomes.me>
2024-02-03 10:23:16 -0300

    Revert "fix: auth route"
    This reverts commit f68a024fa6352e9c0368b47d0459510022cc9a63.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,39 @@
-import authOptions from "@/app/_lib/auth-options";
-import NextAuth from "next-auth";
+import { db } from "@/app/_lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth, { AuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
+import AppleProvider from "next-auth/providers/apple";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(db) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+    // AppleProvider({
+    //   clientId: process.env.APPLE_CLIENT_ID,
+    //   clientSecret: {
+    //     appleId: process.env.APPLE_ID,
+    //     teamId: process.env.APPLE_TEAM_ID,
+    //     privateKey: process.env.APPLE_PRIVATE_KEY,
+    //     keyId: process.env.APPLE_KEY_ID,
+    //   },
+    // }),
+  ],
+  callbacks: {
+    async session({ session, user }) {
+      session.user = { ...session.user, id: user.id } as {
+        id: string;
+        name: string;
+        email: string;
+      };
+
+      return session;
+    },
+  },
+};
 
 const handler = NextAuth(authOptions);
 
